a8ea633dc38cbac38e3f175667c50c5fbdb568ec
taichanNE30 <ne300209@senshu-u.jp>
2018-09-02 17:20:10 +0900

    ランダム返信機能の実装

--- bot.rb
+++ bot.rb
@@ -19,6 +19,69 @@ end
 
 @followers = @client.follower_ids(@my_id).take(7500)
 
+def randomWordsWhenCalled
+    random = Random.new()
+    num = random.rand(0,5)
+    case num
+    when 0
+        return "どうしたの〜〜？？"
+    when 1
+        return "なにか用あるの？？"
+    when 2
+        return "呼んだ？？"
+    when 3
+        return "はい！りんごちゃんです！！"
+    when 4
+        return "（…呼ばれた気がする…）"      
+    else
+        return "なになに？？"
+    end
+end
+
+def randomWordsToReply
+    random = Random.new()
+    num = random.rand(0,10)
+    case num
+    when 0
+        return "そうだよねー"
+    when 1
+        return "そうなんだ！！"
+    when 2
+        return "ふーん"
+    when 3
+        return "なるほどねーーー"
+    when 4
+        return "わかった！（わかってない）"      
+    when 5
+        return "すごい"
+    when 6
+        return "さすが"
+    when 7
+        return "知らなかった！"
+    when 8
+        return "それな"
+    when 9
+        return "わかる"
+    when 10
+        return "せやな"
+    end
+end
+
+def responseToTweet (tweet)
+    puts "\e[33m" + tweet.user.name + "\e[32m" + "[ID:" + tweet.user.screen_name + "]"
+    puts "\e[0m" + tweet.text
+    
+    if tweet.text.include?("限何時")
+        @client.update("@#{tweet.user.screen_name}\n1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\nですよ〜〜！！", options = {:in_reply_to_status_id => tweet.id})
+    elsif tweet.text.include?("ば終わ") || tweet.text.include?("バおわ")||tweet.text.include?("バオワ")||tweet.text.include?("ばおわ")
+        @client.update("@#{tweet.user.screen_name}\nバイトおつかれさま！！今日もがんばったね！！", options = {:in_reply_to_status_id => tweet.id})
+    elsif tweet.text.include?("りんごちゃん")
+        @client.update("@#{tweet.user.screen_name}\n"+randomWordsWhenCalled, options = {:in_reply_to_status_id => tweet.id})
+    elsif tweet.in_reply_to_user_id == @my_id
+        @client.update("@#{tweet.user.screen_name}\n"+randomWordsToReply, options = {:in_reply_to_status_id => tweet.id})
+    end
+end
+
 def homeTimeline_REST
     tl_tweets= @client.home_timeline(count: 200,since_id: @last_tweet_id_REST)
     tl_tweets.reverse.each_with_index do |tweet, index|
@@ -26,24 +89,12 @@ def homeTimeline_REST
             @last_tweet_id_REST = tweet.id
         end
         if tweet.user.protected?
-            puts "\e[33m" + tweet.user.name + "\e[32m" + "[ID:" + tweet.user.screen_name + "]"
-            puts "\e[0m" + tweet.text
-            
-            if tweet.text.include?("りんごちゃん")
-                @client.update("@#{tweet.user.screen_name}\nどうしたの〜〜？？", options = {:in_reply_to_status_id => tweet.id})
-            end
-            if tweet.text.include?("限何時")
-                @client.update("@#{tweet.user.screen_name}\n1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\nですよ〜〜！！", options = {:in_reply_to_status_id => tweet.id})
-            end
-            if tweet.text.include?("ば終わ") || tweet.text.include?("バおわ")||tweet.text.include?("バオワ")||tweet.text.include?("ばおわ")
-                @client.update("@#{tweet.user.screen_name}\nバイトおつかれさま！！今日もがんばったね！！", options = {:in_reply_to_status_id => tweet.id})
-            end
+            responseToTweet(tweet)
         end
     end
     @followers = @client.follower_ids(@my_id).take(7500)
     sleep(60)
 end
-
 
 def arrayToCSV (array)
     objs = ""
@@ -59,19 +110,8 @@ end
 
 def streaming
     @client_Stream.filter(follow:arrayToCSV(@followers)) do |tweet|
-        if tweet.is_a?(Twitter::Tweet) &&tweet.user.id != @my_id
-            puts "\e[33m" + tweet.user.name + "\e[32m" + "[ID:" + tweet.user.screen_name + "]"
-            puts "\e[0m" + tweet.text
-            
-            if tweet.text.include?("りんごちゃん")
-                @client.update("@#{tweet.user.screen_name}\nどうしたの〜〜？？", options = {:in_reply_to_status_id => tweet.id})
-            end
-            if tweet.text.include?("限何時")
-                @client.update("@#{tweet.user.screen_name}\n1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\nですよ〜〜！！", options = {:in_reply_to_status_id => tweet.id})
-            end
-            if tweet.text.include?("ば終わ") || tweet.text.include?("バおわ")||tweet.text.include?("バオワ")||tweet.text.include?("ばおわ")
-                @client.update("@#{tweet.user.screen_name}\nバイトおつかれさま！！今日もがんばったね！！", options = {:in_reply_to_status_id => tweet.id})
-            end
+        if tweet.is_a?(Twitter::Tweet) && tweet.user.id != @my_id
+            responseToTweet(tweet)
         end
     end 
 end
