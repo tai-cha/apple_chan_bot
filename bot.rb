231573df1750a6f9510307f5a716599eb81d7666
taichanNE30 <ne300209@senshu-u.jp>
2018-10-17 17:40:06 +0900

    stream廃止、配列でリプ返し、食堂何時

--- bot.rb
+++ bot.rb
@@ -1,6 +1,11 @@
 require 'twitter'
 
-@last_tweet_id_REST = [(ENV['REST_last_tweet']).to_i, 1].max
+if File.exist?('followers.txt')
+    @last_tweet_id_REST = [(File.read('followers.txt')).to_i, 1].max
+else
+    @last_tweet_id_REST = 1
+end
+
 @my_id = 1004213238379130880
 
 @client = Twitter::REST::Client.new do |config|
@@ -10,125 +15,34 @@ require 'twitter'
     config.access_token_secret = "0SWuWZjTK0xsaKF8rvEAduwlsJlyzcpvWHqYgp8Y1zYnU"
 end
 
-created_NewStreamingClient = false
-
-def createNewStreamingClient
-    @client_Stream = Twitter::Streaming::Client.new do |config|
-        config.consumer_key        = "86tasAy3Cj8XToGE5L29opna3"
-        config.consumer_secret     = "rfdrTfp5pgMmRd4OWXvCQ2UWD1i8lDEgjaqGVnJolKZUoZZJdy"
-        config.access_token        = "1004213238379130880-G4HBMAy0Pb16JjskoR48KV7y4XJS9y"
-        config.access_token_secret = "0SWuWZjTK0xsaKF8rvEAduwlsJlyzcpvWHqYgp8Y1zYnU"
-    end
-    created_NewStreamingClient = true
+def randomFromArray(Array)
+    random = Random.new()
+    return strs[random.rand(0..(strs.length - 1))]
 end
 
-createNewStreamingClient
-
-@followers = @client.follower_ids(@my_id).take(7500)
-
 def randomWordsWhenCalled
-    random = Random.new()
-    num = random.rand(0..5)
-    case num
-    when 0
-        return "どうしたの〜〜？？"
-    when 1
-        return "なにか用あるの？？"
-    when 2
-        return "呼んだ？？"
-    when 3
-        return "はい！りんごちゃんです！！"
-    when 4
-        return "（…呼ばれた気がする…）"      
-    else
-        return "なになに？？"
-    end
+    strs = ["どうしたの〜〜？？", "なにか用あるの？？", "呼んだ？？", "はい！りんごちゃんです！！", "（…呼ばれた気がする…）", "なになに？？"]
+    return randomFromArray(strs)
 end
 
 def randomWordsToReply
-    random = Random.new()
-    num = random.rand(0..15)
-    case num
-    when 0
-        return "そうだよねー"
-    when 1
-        return "そうなんだ！！"
-    when 2
-        return "ふーん"
-    when 3
-        return "なるほどねーーー"
-    when 4
-        return "わかった！（わかってない）"      
-    when 5
-        return "すごい"
-    when 6
-        return "さすが"
-    when 7
-        return "知らなかった！"
-    when 8
-        return "それな"
-    when 9
-        return "わかる"
-    when 10
-        return "せやな"
-    when 11
-        return "ありがとう！！"
-    when 12
-        return "それ好き"
-    when 13
-        return "天才か？？"
-    when 14
-        return "それは神"
-    when 15
-        return "うれしい！！！！！"
-    end
+    strs = ["そうだよねー","そうなんだ！！","ふーん","なるほどねーーー","わかった！（わかってない）","すごい","さすが","知らなかった！","それな","わかる","せやな","ありがとう！！","それ好き","天才か？？","それは神","うれしい！！！！！"]
+    return randomFromArray(strs)
 end
 
 def randomWordsSleepy
-    random = Random.new()
-    num = random.rand(0..2)
-    case num
-    when 0
-        return "おやすみなさい！！"
-    when 1
-        return "無理はしないでね！！"
-    when 2
-        return "はやく寝なよ！！"
-    end
+    strs = ["おやすみなさい！！", "無理はしないでね！！", "はやく寝なよ！！"]
+    return randomFromArray(strs)
 end
 
 def randomWordsLike
-    random = Random.new()
-    num = random.rand(0..4)
-    case num
-    when 0
-        return "好き！！"
-    when 1
-        return "大好き！！"
-    when 2
-        return "わかる！！"
-    when 3
-        return "めーっちゃ好き"
-    when 4
-        return "えーーーー"
-    end
+    strs = ["好き！！", "大好き！！", "わかる！！", "めーっちゃ好き", "えーーーー"]
+    return randomFromArray(strs)
 end
 
 def randomWordsWhenThanks
-    random = Random.new()
-    num = random.rand(0..4)
-    case num
-    when 0
-        return "どういたしまして！！"
-    when 1
-        return "えへへ..."
-    when 2
-        return "天才なので"
-    when 3
-        return "こちらこそ！！！"
-    when 4
-        return "うれしい！！！！"
-    end
+    str = ["どういたしまして！！", "えへへ...", "天才なので", "こちらこそ！！！", "うれしい！！！！"]
+    return randomFromArray(strs)
 end
 
 def responseToTweet (tweet)
@@ -139,10 +53,20 @@ def responseToTweet (tweet)
             
             if tweet.text.include?("限何時")
                 @client.update("@#{tweet.user.screen_name}\n1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\nですよ〜〜！！", options = {:in_reply_to_status_id => tweet.id})
+            elsif tweet.text.include?("食堂何時")
+                @client.update("@#{tweet.user.screen_name}\n専修の生田キャンパスの食堂の営業時間です！\nhttp://pic.twitter.com/rVjDU4cODv")
             elsif tweet.text.include?("ば終わ") || tweet.text.include?("バおわ")||tweet.text.include?("バオワ")||tweet.text.include?("ばおわ")
                 @client.update("@#{tweet.user.screen_name}\nバイトおつかれさま！！今日もがんばったね！！", options = {:in_reply_to_status_id => tweet.id})
-            elsif tweet.text.include?("おはよう")|| tweet.text.include?("いまおきた")|| tweet.text.include?("今起きました")|| tweet.text.include?("いまおきました") || tweet.text == "ぽ"
+            elsif tweet.text.include?("おはよう")|| tweet.text.include?("いまおきた")|| tweet.text.include?("今起きました")|| tweet.text.include?("いまおきました")
                 @client.update("@#{tweet.user.screen_name}\nおはよう！！", options = {:in_reply_to_status_id => tweet.id})
+            elsif tweet.text == "ぽ"
+                currentTime = Time.now.localtime("+09:00")
+                currentHour = currentTime.strftime("%H").to_i
+                if(currentHour < 4, currentHour >= 17)
+                    @client.update("@#{tweet.user.screen_name}\nおやすみ！！", options = {:in_reply_to_status_id => tweet.id})
+                else
+                    @client.update("@#{tweet.user.screen_name}\nおはよう！！", options = {:in_reply_to_status_id => tweet.id})
+                end
             elsif tweet.text.include?("ねていい？")|| tweet.text.include?("ねていい?")
                 random = Random.new()
                 num = random.rand(0..1)
@@ -199,12 +123,12 @@ def homeTimeline_REST
     tl_tweets.reverse.each_with_index do |tweet, index|
         if index == tl_tweets.size - 1
             @last_tweet_id_REST = tweet.id
-            ENV['REST_last_tweet'] = @last_tweet_id_REST.to_s
+            File.open('last_tweet_id.txt',"w") do |file|
+                file.print(@last_tweet_id_REST.to_s)
+                file.close
+            end
         end
         responseToTweet(tweet)
-        # if tweet.user.protected?
-        #     responseToTweet(tweet)
-        # end
     end
     sleep(60)
     @followers = @client.follower_ids(@my_id).take(7500)
@@ -215,67 +139,6 @@ def homeTimeline_REST
     end
 end
 
-def arrayToCSV (array)
-    objs = ""
-    array.each_with_index do |value, index|
-        if index == array.size - 1
-            objs += value.to_s
-            break
-        end
-        objs += value.to_s+","
-    end
-    return objs
-end
-
-def isFollowersDiff ()
-    if File.read('followers.txt')[2..-2].split(", ").map(&:to_i) == @followers
-        return false
-    else
-        @followers = File.read('followers.txt')[2..-2].split(", ").map(&:to_i)
-        return true
-    end
-end
-
-def streaming
-    begin
-        @client_Stream.filter(follow:arrayToCSV(@followers)) do |tweet|
-            if tweet.is_a?(Twitter::Tweet) && tweet.user.id != @my_id
-                responseToTweet(tweet)
-            end
-            if isFollowersDiff()
-                puts "followers changed"
-                break
-                return 0
-            end
-        end
-        puts "処理切った"
-        return 0
-    rescue => error
-        puts "エラーが起きました"
-        puts error.message
-        puts ""
-        if error.message.include?("765: ")
-            sleep(5)
-        end
-    end
-end
-
-# @pid = fork do
-#     loop do
-#         homeTimeline_REST
-#     end
-# end
-
 loop do
     homeTimeline_REST
 end
-
-# loop do
-#     puts "ストリーミング始めるよ"
-#     created_NewStreamingClient
-#     streaming
-# end
-
-# at_exit do
-#     system("kill #{@pid}")
-# end
--- test.rb
+++
@@ -1,5 +0,0 @@
-@followers_str = File.read('followers.txt')
-@followers = @followers_str[2..-2].split(", ")
-@followers = @followers.map(&:to_i)
-
-puts @followers.to_s
