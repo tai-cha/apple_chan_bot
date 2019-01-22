32da5b18c14bb52c7b9fd98e94ac0433575e8dc6
taichanNE30 <ne300209@senshu-u.jp>
2019-01-22 21:51:59 +0900

    [fix] reply()がツイートを取得できない問題

--- bot.rb
+++ bot.rb
@@ -28,7 +28,7 @@ def randomFromArray(array)
     return array[random.rand(0..(array.length - 1))]
 end
 
-def reply(message="")
+def reply(tweet,message="")
     tweet_text = "@#{tweet.user.screen_name}\n#{message}"
     @client.update(tweet_text, options = {:in_reply_to_status_id => tweet.id})
 end
@@ -51,79 +51,79 @@ def responseToTweet (tweet)
         if (!tweet.text.include?("@") || tweet.text.include?("@apple_chan_bot")) && tweet.user.screen_name != "apple_chan_bot"
             
             if tweet.text.include?("限何時")
-                reply("1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\n6限18:15~19:45\n7限19:55~21:25\nですよ〜〜！！")
+                reply(tweet, "1限9:00-10:30\n2限10:45-12:15\n3限13:05-14:35\n4限14:50-16:20\n5限16:35-18:05\n6限18:15~19:45\n7限19:55~21:25\nですよ〜〜！！")
             elsif tweet.text.include?("食堂何時")
-                reply("専修の生田キャンパスの食堂の営業時間です！\nhttp://pic.twitter.com/rVjDU4cODv")
+                reply(tweet, "専修の生田キャンパスの食堂の営業時間です！\nhttp://pic.twitter.com/rVjDU4cODv")
             elsif tweet.text.include?("ば終わ") || tweet.text.include?("バおわ")||tweet.text.include?("バオワ")||tweet.text.include?("ばおわ")
-                reply("バイトおつかれさま！！今日もがんばったね！！")
+                reply(tweet, "バイトおつかれさま！！今日もがんばったね！！")
             elsif tweet.text.include?("おはよう")|| tweet.text.include?("いまおきた")|| tweet.text.include?("今起きました")|| tweet.text.include?("いまおきました")
-                reply("おはよう！！")
+                reply(tweet, "おはよう！！")
             elsif tweet.text == "ぽ"
                 currentTime = Time.now.localtime("+09:00")
                 currentHour = currentTime.strftime("%H").to_i
                 if(currentHour < 4 || currentHour >= 17)
-                    reply("おやすみ！！")
+                    reply(tweet, "おやすみ！！")
                 else
-                    reply("おはよう！！")
+                    reply(tweet, "おはよう！！")
                 end
             elsif tweet.text.include?("ねていい？")|| tweet.text.include?("ねていい?")
                 random = Random.new()
                 num = random.rand(0..1)
                 case num
                 when 0
-                    reply("いいよ！！おやすみ！！")
+                    reply(tweet, "いいよ！！おやすみ！！")
                 when 1
-                    reply("ダメだよ！！おきて！！")
+                    reply(tweet, "ダメだよ！！おきて！！")
                 end
             elsif tweet.text.include?("こんにちは")
-                reply("こんにちは！！")
+                reply(tweet, "こんにちは！！")
             elsif tweet.text.include?("ねむい") || tweet.text.include?("眠い")
-                reply(""+randomWordsSleepy)
+                reply(tweet, ""+randomWordsSleepy)
             elsif tweet.text.include?("おやすみ")
-                reply("おやすみ〜〜〜")    
+                reply(tweet, "おやすみ〜〜〜")    
             elsif tweet.text.include?("絶起")
-                reply("ちゃんと起きましょうね！！！")  
+                reply(tweet, "ちゃんと起きましょうね！！！")  
             elsif tweet.text.include?("魔剤")
-                reply("美味しいけど飲み過ぎには注意ですよ！")  
+                reply(tweet, "美味しいけど飲み過ぎには注意ですよ！")  
             elsif tweet.text.include?("1限チャレンジ") || tweet.text.include?("一限チャレンジ")
-                reply("またギリギリですか？ちゃんと学校間に合ってくださいよ？")
+                reply(tweet, "またギリギリですか？ちゃんと学校間に合ってくださいよ？")
             elsif tweet.text.include?("つかれた") || tweet.text.include?("疲れた")
-                reply("今日もお疲れ様！！")
+                reply(tweet, "今日もお疲れ様！！")
             elsif tweet.text.include?("頑張る") || tweet.text.include?("がんばる")
-                reply("がんばれ！！ファイトーーー！！")
+                reply(tweet, "がんばれ！！ファイトーーー！！")
             elsif tweet.text.include?("ちんちん") || tweet.text.include?("おっぱい") || tweet.text.include?("ちんこ") || tweet.text.include?("まんこ")
                 random = Random.new()
                 num = random.rand(0..100)
                 case num
                 when 0..30
-                    reply("そ、そんなエッチなこと言っちゃダメなんだからね！！！", option = {:in_reply_to_status_id => tweet.id}) 
+                    reply(tweet, "そ、そんなエッチなこと言っちゃダメなんだからね！！！", option = {:in_reply_to_status_id => tweet.id}) 
                 when 31..100
-                    reply("セクハラはよくないと思います……")
+                    reply(tweet, "セクハラはよくないと思います……")
                 end
             elsif tweet.text.include?("進捗ダメ") || tweet.text.include?("進捗ありません") || tweet.text.include?("進捗ない")
-                reply("明日は進捗出るよ！頑張って！！")
+                reply(tweet, "明日は進捗出るよ！頑張って！！")
             elsif tweet.text.include?("バ行")
-                reply("バイト頑張ってください！")
+                reply(tweet, "バイト頑張ってください！")
             elsif tweet.text.include?("課題終わった")|| tweet.text.include?("課題おわった")
-                reply("おつかれさま！！今日はゆっくり寝よう〜〜！")
+                reply(tweet, "おつかれさま！！今日はゆっくり寝よう〜〜！")
             elsif tweet.text.include?("りんごちゃん") && tweet.text.include?("お前もそう思うだろ")
                 @client.favorite(tweet, options={})
-                reply("そうなのだ！！！")
+                reply(tweet, "そうなのだ！！！")
             elsif tweet.text.include?("りんごちゃん")
                 @client.favorite(tweet, options={})
-                reply(randomFromArray(WHEN_CALLED))
+                reply(tweet, randomFromArray(WHEN_CALLED))
             elsif tweet.in_reply_to_user_id == MY_ID
                 @client.favorite(tweet, options={})
                 if tweet.text.include?("天気")
-                    reply(weather_yokohama)
+                    reply(tweet, weather_yokohama)
                 elsif tweet.text.include?("好き")||tweet.text.include?("すき")
-                    reply(randomFromArray(WHEN_LIKE))
+                    reply(tweet, randomFromArray(WHEN_LIKE))
                 elsif tweet.text.include?("ありがと")
-                    reply(randomFromArray(WHEN_THANKS))
+                    reply(tweet, randomFromArray(WHEN_THANKS))
                 elsif tweet.text.include?("聞いて") ||tweet.text.include?("きいて")
-                    reply(randomFromArray(WHEN_CALLED))
+                    reply(tweet, randomFromArray(WHEN_CALLED))
                 else
-                    reply(randomFromArray(WHEN_REPLYED))
+                    reply(tweet, randomFromArray(WHEN_REPLYED))
                 end
             end
         end
